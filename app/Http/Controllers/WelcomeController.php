9d847f9a293caf510e07dd772b44e316ccd6d1ae
Michal382 <michal.zyw0@gmail.com>
2022-12-08 18:16:28 +0100

    Kategorie produktow

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Exception;
+use App\Models\ProductCategory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class WelcomeController extends Controller
@@ -19,7 +21,9 @@ class WelcomeController extends Controller
     public function index(): View
     {
         return view("welcome", [
-            'products' => Product::paginate(10)
+            'products' => Product::paginate(10),
+            'categories' => ProductCategory::orderBy('name', 'ASC')->get(),
+
         ]);
     }
 }
